e21a54bb0095122276bfd086f3266471e4384907
rajibulislam60 <rajibulislam60707877@gmail.com>
2024-05-13 22:46:41 +0600

    react start work

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+function App(){
+  return(
+    <>
+    <h1>Rajibul Islam</h1>
+    <h2>me</h2>
+    </>
+  )
+}
+
+export default App
---
+++ src/Navbar.jsx
@@ -0,0 +1,9 @@
+function Navbar(){
+    return (
+        <>
+            <h1>Rajibul Islam Rajib</h1>
+        </>
+    )
+}
+
+export default Navbar
